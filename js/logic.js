a9de4ff69ffcb1dd6afc2fd57d10f2bd420eba9b
Christella De Sousa <cde-sous@f6r7s15.clusters.42paris.fr>
2025-06-18 10:27:28 +0200

    delete comment

--- js/logic.js
+++ js/logic.js
@@ -81,8 +81,7 @@ function enableEditingTask(span) {
 function disableEditingTask(input, tasks, taskIdx, initialText) {
 	const newText = input.value.trim();
 	if (newText !== '' && input.value !== initialText)
-	{// rename filterList et filterTasks (logic.js et ui.js)
-
+	{
 		tasks[taskIdx].text = newText;
 		tasks[taskIdx].completed = false;
 	}
